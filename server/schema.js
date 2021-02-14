fd25c4ddccce56fdb78e666ce7e3bf9542bc79da
Pahomkch <Pahomov.kch@gmail..com>
2021-02-14 23:07:17 +0500

    add graphqlHTTP to server with resolver and schema. w/o data base

---
+++ server/schema.js
@@ -0,0 +1,38 @@
+const { buildSchema } = require('graphql');
+
+const schema = buildSchema(`
+  type User {
+    id: ID
+    username: String
+    age: Int
+    posts: [Post]
+  }
+
+  type Post {
+    id: ID
+    title: String
+    content: String
+  }
+
+  input UserInput {
+    username: String!
+    age: Int!
+    Posts: [PostInput]
+  }
+
+  input PostInput {
+    id: ID
+    title: String!
+    content: String!
+  }
+
+  type Query {
+    getAllUsers: [User]
+    getUser(id: ID): User
+  }
+
+
+
+`);
+
+module.exports = schema;
--- server/server.js
+++ server/server.js
@@ -1,8 +1,35 @@
 require('dotenv').config();
 const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors');
+const schema = require('./schema');
+
+const bdInFuture = [
+  {
+    id: 1,
+    username: 'Andrew',
+    age: 27,
+    posts: [
+      {
+        id: 100,
+        title: 'Hello',
+        content: 'Text Post',
+      },
+    ],
+  },
+];
 
 const app = express();
+app.use(cors());
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
 
 app.listen(process.env.PORT, () => {
-  console.log(`server is run`);
+  console.log(`server is run on port ${process.env.PORT}`);
 });
